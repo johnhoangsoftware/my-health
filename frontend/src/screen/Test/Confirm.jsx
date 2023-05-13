7c9686ca714c265ee1ef743eeef56315f42edc50
johnhoangsoftware <johnhoangsoftware@gmail.com>
2023-05-14 03:22:24 +0700

    feat(api): 📱 confirm test package

--- frontend/src/screen/Test/Confirm.jsx
+++ frontend/src/screen/Test/Confirm.jsx
@@ -1,9 +1,31 @@
 import { View, Text, TouchableOpacity, Modal, Pressable } from "react-native";
 import Profile from "../../component/Utils/Profile";
 import React from "react";
+import useAxios from "../../hooks/useAxios";
 
 export default function ConfirmTest({ navigation, route }) {
     const { profile, testPackage, date, hour } = route.params;
+    const  [appointmentStatus, setAppointmentStatus ] = React.useState(false);
+    const [modalVisible, setModalVisible] = React.useState(false);
+    const axios = useAxios();
+    const handleAppointment = () => {
+        axios.post(`/patient/appointment`, {
+            date: date.shortDate,
+            time: hour,
+            testPackageId: testPackage.id,
+            medicalRecordId: profile.id
+        }
+        )
+            .then(res => {
+                if (res.status < 400) {
+                    setAppointmentStatus(true);
+                    
+                }
+            })
+            .catch(err => {
+                console.log(JSON.stringify(err))
+            })
+    }
 
     const Info = (props) => {
         return (
@@ -34,7 +56,7 @@ export default function ConfirmTest({ navigation, route }) {
         )
     }
 
-    const [modalVisible, setModalVisible] = React.useState(false);
+    // const [modalVisible, setModalVisible] = React.useState(false);
 
     return (
         <View>
@@ -55,10 +77,15 @@ export default function ConfirmTest({ navigation, route }) {
             />
             <TouchableOpacity
                 className="m-auto w-1/3 p-2 mt-4 mb-8 rounded" style={{ backgroundColor: "#24DCE2" }}
-                onPress={() => setModalVisible(true)}
+                onPress={() => {
+                    handleAppointment();
+                    setModalVisible(true);
+                }
+                }
             >
                 <Text className="text-white font-bold text-center">Xác nhận</Text>
             </TouchableOpacity>
+
             <Modal
                 animationType="fade"
                 transparent={true}
@@ -70,7 +97,7 @@ export default function ConfirmTest({ navigation, route }) {
                 <View className="w-screen h-screen items-center justify-center">
                     <View className="w-screen h-screen bg-black opacity-25 absolute"></View>
                     <View className="bg-white items-center justify-center w-4/5 py-4 px-3.5 rounded-md shadow-sm">
-                        <Text className="font-bold text-lg">Đặt lịch xét nghiệm thành công!</Text>
+                        {appointmentStatus?<Text className="font-bold text-lg">Đặt lịch xét nghiệm thành công!</Text>:<Text className="font-bold text-lg">Đặt lịch xét nghiệm thất bại!</Text>}
                         <Pressable
                             className="mt-4 p-2 rounded"
                             style={{ backgroundColor: "#24DCE2" }}
